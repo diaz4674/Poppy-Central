e6b3789bcd969c63bdb1bac38ba6a9919a869ca0
Miguel Diaz <diaz4674@gmail.com>
2020-11-25 22:41:10 +0000

    pretty af

--- poppy-central/src/App.js
+++ poppy-central/src/App.js
@@ -9,211 +9,308 @@ import TextField from "@material-ui/core/TextField";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import Button from "@material-ui/core/Button";
 import download from "downloadjs";
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		"& > *": {
-			margin: theme.spacing(1),
-			width: "25ch",
-		},
-		button: {
-			margin: theme.spacing(1),
-		},
-	},
+    root: {
+        "& > *": {
+            margin: theme.spacing(1),
+            width: "25ch",
+        },
+        button: {
+            margin: theme.spacing(1),
+        },
+    },
 }));
 
-		// AccountInfo: {
-		// 	Type: "Business",
-		// 	Ownership: "CCorp",
-		// 	Benificiary: "",
-		// 	BeneficiaryDetails: "",
-		// 	totalSigners: 3,
-		// 	BusinessName: "",
-		// 	Prefix: "",
-		// 	PrefixName: "",
-		// 	AnotherName: "Welltower Redmond",
-		// 	Street: "",
-		// 	City: "",
-		// 	EIN: "",
-		// 	AccountType1: "",
-		// 	AccountNumber1: "",
-		// },
+// AccountInfo: {
+// 	Type: "Business",
+// 	Ownership: "CCorp",
+// 	Benificiary: "",
+// 	BeneficiaryDetails: "",
+// 	totalSigners: 3,
+// 	BusinessName: "",
+// 	Prefix: "",
+// 	PrefixName: "",
+// 	AnotherName: "Welltower Redmond",
+// 	Street: "",
+// 	City: "",
+// 	EIN: "",
+// 	AccountType1: "",
+// 	AccountNumber1: "",
+// },
 
 class App extends Component {
-	state = {
-		AccountInfo: {
-			Type: "Business",
-			Ownership: "CCorp",
-			Benificiary: "",
-			BeneficiaryDetails: "",
-			totalSigners: 3,
-			BusinessName: "Netlix",
-			Prefix: "DBA",
-			PrefixName: "Hulu",
-			AnotherName: "",
-			Street: "123 Main St",
-			City: "Santa Rosa, CA 94949",
-			EIN: "12-345676",
-			AccountType1: "Business Checking",
-			AccountNumber1: "01-1000088-8",
-		},
-	};
+    state = {
+        AccountInfo: {
+            Type: "Business",
+            Ownership: "CCorp",
+            Benificiary: "",
+            BeneficiaryDetails: "",
+            totalSigners: 3,
+            BusinessName: "Netlix",
+            Prefix: "",
+            PrefixName: "Hulu",
+            AnotherName: "",
+            Street: "123 Main St",
+            City: "Santa Rosa, CA 94949",
+            EIN: "12-345676",
+            AccountType1: "Business Checking",
+            AccountNumber1: "01-1000088-8",
+        },
+        toggleCheckboxes: {
+            prefix: false,
+            addLine: false,
+            prefixClass: "",
+            addLineClass: "",
+            inputBoxes: ""
+        }
+    };
 
-	updateCardHandler = () => {
-		console.log(this.state)
-		axios
-			.post(
-				// "https://5000-c85a660f-3dbe-4fc9-9c8c-83ea85769df5.ws-us02.gitpod.io/",
-				"http://127.0.0.1:5000/",
-				this.state.AccountInfo,
-				{ responseType: "blob" } // had to add this one here
-			)
-			.then((res) => {
-				download(res.data, "Sig Card", res.content);
+    updateCardHandler = () => {
+        console.log(this.state)
+        axios
+            .post(
+                // "https://5000-c85a660f-3dbe-4fc9-9c8c-83ea85769df5.ws-us02.gitpod.io/",
+                "http://127.0.0.1:5000/",
+                this.state.AccountInfo,
+                { responseType: "blob" } // had to add this one here
+            )
+            .then((res) => {
+                download(res.data, "Sig Card", res.content);
 
-				console.log(res);
-				return res;
-			})
-			.catch((error) => console.log(error));
-		axios
-			.post(
-				// "https://5000-c85a660f-3dbe-4fc9-9c8c-83ea85769df5.ws-us02.gitpod.io/",
-				"http://127.0.0.1:5000/resolution",
-				this.state.AccountInfo,
-				{ responseType: "blob" } // had to add this one here
-			)
-			.then((res) => {
-				download(res.data, "Resolution", res.content);
+                console.log(res);
+                return res;
+            })
+            .catch((error) => console.log(error));
+        axios
+            .post(
+                // "https://5000-c85a660f-3dbe-4fc9-9c8c-83ea85769df5.ws-us02.gitpod.io/",
+                "http://127.0.0.1:5000/resolution",
+                this.state.AccountInfo,
+                { responseType: "blob" } // had to add this one here
+            )
+            .then((res) => {
+                download(res.data, "Resolution", res.content);
 
-				console.log(res);
-				return res;
-			})
-			.catch((error) => console.log(error));
-		console.log("hi");
-	};
+                console.log(res);
+                return res;
+            })
+            .catch((error) => console.log(error));
+        console.log("hi");
+    };
+    handleCheckboxChange = (e) => {
+        this.state.toggleCheckboxes[e.target.name] = !this.state.toggleCheckboxes[e.target.name];
+        let { toggleCheckboxes } = this.state
 
-	handleChange = (e) => {
-		// let stateName = e.target.name
-		// console.log(stateName)
-		// JSON.stringify(stateName)
+        this.setState({ toggleCheckboxes })
+        console.log(toggleCheckboxes)
+        if (e.target.name === "prefix") {
+            if (toggleCheckboxes.prefix) {
+                this.setState({
+                    // ...this.state.toggleCheckboxes, 
+                    toggleCheckboxes: {
+                        ...this.state.toggleCheckboxes,
+                        prefixClass: "prefix",
+                        inputBoxes: "inputBoxes",
+                        addLineOption: "addLineOption"
+                    }
+                })
+            } else {
+                this.setState({
+                    // ...this.state.toggleCheckboxes, 
+                    toggleCheckboxes: {
+                        prefixClass: ""
+                    }
+                })
+            }
 
-		this.state.AccountInfo[e.target.name] = e.target.value;
-		let {AccountInfo} = this.state
-		console.log(this.state)
-		this.setState({ AccountInfo });
-		console.log(this.state);
-	};
-	render() {
-		return (
-			<div className="InputBox">
-				<img src={logo} className="logo" />
-				<div>
-					<TextField
-						id="outlined-basic"
-						value={this.state.BusinessName}
-						name="BusinessName"
-						onChange={this.handleChange}
-						label="Business Name"
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Prefix"
-						name="Prefix"
-						value={this.state.Prefix}
-						onChange={this.handleChange}
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="PrefixName"
-						name="PrefixName"
-						value={this.state.PrefixName}
-						onChange={this.handleChange}
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Add Another Name"
-						name="AnotherName"
-						value={this.state.AnotherName}
-						onChange={this.handleChange}
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Street"
-						multiline
-						name="Street"
-						value={this.state.Street}
-						onChange={this.handleChange}
-						placeholder="123 Happy St."
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="City"
-						name="City"
-						value={this.state.City}
-						onChange={this.handleChange}
-						multiline
-						placeholder="Santa Rosa, CA 94949"
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						name="EIN"
-						value={this.state.EIN}
-						onChange={this.handleChange}
-						label="EIN"
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Account Type"
-						name={"AccountType1"}
-						value={this.state.AccountType1}
-						onChange={this.handleChange}
-						variant="outlined"
-						className="inputBoxes"
-					/>
-					<TextField
-						id="outlined-basic"
-						label="Account Number"
-						name={"AccountNumber1"}
-						value={this.state.AccountType1}
-						onChange={this.handleChange}
-						multiline
-						variant="outlined"
-						placeholder="Business Checking"
-						className="inputBoxes"
-					/>
-				</div>
-				<button onClick={this.updateCardHandler}>Change Sigs</button>
-				<div className="buttonContainer">
-					<Button
-						variant="contained"
-						color="default"
-						// onClick={this.updateCardHandler}
-						className="submitButton"
-					>
-						Generate Signature Card
+        } else {
+            if (toggleCheckboxes.addLine) {
+                this.setState({
+                    // ...this.state.toggleCheckboxes, 
+                    toggleCheckboxes: {
+                        ...this.state.toggleCheckboxes,
+                        addLineClass: "addLineClass",
+                        inputBoxes: "inputBoxes"
+                    }
+                })
+            } else {
+                this.setState({
+                    // ...this.state.toggleCheckboxes, 
+                    toggleCheckboxes: {
+                        ...this.state.toggleCheckboxes,
+                        addLineClass: "",
+                    }
+                })
+            }
+        }
+        // console.log(this.state.toggleCheckboxes)
+    }
+
+    handleChange = (e) => {
+        // let stateName = e.target.name
+        // console.log(stateName)
+        // JSON.stringify(stateName)
+
+        this.state.AccountInfo[e.target.name] = e.target.value;
+        let { AccountInfo } = this.state
+        console.log(this.state)
+        this.setState({ AccountInfo });
+        console.log(this.state);
+    };
+    render() {
+        let { prefixClass, inputBoxes, prefix, addLineOption, addLine, addLineClass } = this.state.toggleCheckboxes
+        return (
+            <div className="InputBox">
+                <div style={{ display: 'flex', justifyContent: "center", width: "100%" }}>
+                    <img src={logo} className="logo" />
+                </div>
+                <div>
+                    <TextField
+                        id="outlined-basic"
+                        value={this.state.BusinessName}
+                        name="BusinessName"
+                        onChange={this.handleChange}
+                        label="Business Name"
+                        variant="outlined"
+
+                        className="inputBoxes"
+                    />
+                    {/* Prefix Check  */}
+                    <FormGroup aria-label="position" className="checkBoxDiv" row style={{ padding: "0 35px" }}>
+                        <FormControlLabel
+                            value={prefix}
+                            control={<Checkbox />}
+                            style={{ color: "grey" }}
+                            label="Prefix"
+                            name="prefix"
+                            onChange={this.handleCheckboxChange}
+                            labelPlacement="start"
+                            className="checkBox"
+                        />
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            name="Prefix"
+                            value={this.state.Prefix}
+                            onChange={this.handleChange}
+                            style={{ width: "150px", marginLeft: "25px" }}
+                            className={`noShow ${prefixClass}`}
+                        >
+                            <MenuItem value={"DBA"}>DBA</MenuItem>
+                            <MenuItem value={"FBO"}>FBO</MenuItem>
+                        </Select>
+                    </FormGroup>
+
+                    <TextField
+                        id="outlined-basic"
+                        label="Prefix Name"
+                        name="PrefixName"
+                        value={this.state.PrefixName}
+                        onChange={this.handleChange}
+                        variant="outlined"
+                        className={`noShow ${prefixClass} ${inputBoxes}`}
+                    />
+                    <FormGroup aria-label="position" className="checkBoxDiv" row style={{ display: 'flex', justifyContent: "flex-start", padding: "0 35px" }}>
+                        <FormControlLabel
+                            value={addLine}
+                            control={<Checkbox />}
+                            name="addLine"
+                            style={{ color: "grey" }}
+                            onChange={this.handleCheckboxChange}
+                            label="Add Line"
+                            labelPlacement="start"
+                            row
+                            className={`noShow ${addLineOption}`}
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="Add Another Name"
+                            name="AnotherName"
+                            value={this.state.AnotherName}
+                            onChange={this.handleChange}
+                            style={{ width: "280px", paddingLeft: "18px !important" }}
+                            variant="outlined"
+                            className={`noShow ${addLineClass} ${inputBoxes}`}
+                        />
+                    </FormGroup>
+                    <TextField
+                        id="outlined-basic"
+                        name="EIN"
+                        value={this.state.EIN}
+                        onChange={this.handleChange}
+                        label="EIN"
+                        variant="outlined"
+                        className="inputBoxes"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Account Number"
+                        name={"AccountNumber1"}
+                        value={this.state.AccountType1}
+                        onChange={this.handleChange}
+                        multiline
+                        variant="outlined"
+                        className="inputBoxes"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Account Type"
+                        name={"AccountType1"}
+                        placeholder="Business Checking"
+                        value={this.state.AccountType1}
+                        onChange={this.handleChange}
+                        variant="outlined"
+                        className="inputBoxes"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Street"
+                        multiline
+                        name="Street"
+                        value={this.state.Street}
+                        onChange={this.handleChange}
+                        placeholder="123 Happy St."
+                        variant="outlined"
+                        className="inputBoxes"
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="City"
+                        name="City"
+                        value={this.state.City}
+                        onChange={this.handleChange}
+                        multiline
+                        placeholder="Santa Rosa, CA 94949"
+                        variant="outlined"
+                        className="inputBoxes"
+                    />
+                </div>
+                {/* <button onClick={this.updateCardHandler}>Change Sigs</button> */}
+                <div className="buttonContainer">
+                    <Button
+                        variant="contained"
+                        color="default"
+                        onClick={() => console.log(this.state)}
+                        className="submitButton"
+                    >
+                        Generate Docs
 					</Button>
-				</div>
-			</div>
-		);
-	}
+                </div>
+            </div >
+        );
+    }
 }
 
 App.propTypes = {
-	classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(useStyles)(App);
