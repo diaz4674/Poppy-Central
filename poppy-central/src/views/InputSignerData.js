f8757fe51b3500a47b588cf5c6fc0f57ebba49c5
miguel diaz <diaz4674@gmail.com>
2021-02-02 12:31:13 -0800

    updated input signer info

--- poppy-central/src/views/InputSignerData.js
+++ poppy-central/src/views/InputSignerData.js
@@ -1,23 +1,23 @@
-import React from "react";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import "./HeaderAccount.css";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import { makeStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/styles";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import AddMembers from "./AddMembers";
-import { withRouter } from "react-router-dom";
-import Accordian from "./Accordian";
-import logo from "./logo-mobile.svg";
-import Avatar from "@material-ui/core/Avatar";
-import AvatarGroup from "@material-ui/lab/AvatarGroup";
+import React from "react"
+import Radio from "@material-ui/core/Radio"
+import RadioGroup from "@material-ui/core/RadioGroup"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import FormControl from "@material-ui/core/FormControl"
+import "./HeaderAccount.css"
+import InputLabel from "@material-ui/core/InputLabel"
+import MenuItem from "@material-ui/core/MenuItem"
+import Select from "@material-ui/core/Select"
+import { makeStyles } from "@material-ui/core/styles"
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/styles"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+import AddMembers from "./AddMembers"
+import { withRouter } from "react-router-dom"
+import Accordian from "./Accordian"
+import logo from "./logo-mobile.svg"
+import Avatar from "@material-ui/core/Avatar"
+import AvatarGroup from "@material-ui/lab/AvatarGroup"
 
 const useStyles = makeStyles((theme) => ({
 	formControl: {
@@ -27,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
 	selectEmpty: {
 		marginTop: theme.spacing(2),
 	},
-}));
+}))
 
 class InputSignerData extends React.Component {
 	state = {
@@ -46,33 +46,33 @@ class InputSignerData extends React.Component {
 			},
 		],
 		toggleSigners: false,
-		TeamMembers: [
-			{
-				name: "Sarah Conners",
-			},
-			{
-				name: "Miggy Smallz",
-			},
-			{
-				name: "Jamie Smith",
-			},
-		],
-	};
+	}
+
+	async componentDidMount() {
+		let { inputData } = await this.props.location.state
+		this.setState({
+			...this.state,
+			ProjectName: inputData.ProjectName,
+			TeamMembers: inputData.teamMembers,
+			numSigners: inputData.signers,
+		})
+	}
 
 	handleChange = (e) => {
-		let change = (this.state[e.target.name] = e.target.value);
-		this.setState({ ...this.state, change });
-	};
+		let change = (this.state[e.target.name] = e.target.value)
+		this.setState({ ...this.state, change })
+	}
 
 	getMembers = (members) => {
-		this.setState({ ...this.state, teamMembers: members });
-	};
+		this.setState({ ...this.state, teamMembers: members })
+	}
 
 	handleChangeRadio = (e) => {
-		this.setState({ ...this.state, type: e.target.value });
-	};
+		this.setState({ ...this.state, type: e.target.value })
+	}
 
 	render() {
+		let { ProjectName, TeamMembers } = this.state
 		return (
 			<div className="container">
 				<div
@@ -104,6 +104,17 @@ class InputSignerData extends React.Component {
 						</div>
 						<div
 							style={{
+								width: "100%",
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "flex-start",
+								alignItems: "center",
+							}}
+						>
+							<h1>Project: {ProjectName}</h1>
+						</div>
+						<div
+							style={{
 								display: "flex",
 								flexDirection: "row",
 								justifyContent: "space-between",
@@ -128,38 +139,35 @@ class InputSignerData extends React.Component {
 									<div className="teamMemberBox">
 										<div className="showSigners">
 											{this.state.TeamMembers.map((member) => {
-												return <p>{member.name} </p>;
+												return <p>{member.value} </p>
 											})}
 										</div>
 									</div>
 								) : null}
-								<AvatarGroup max={4}>
-									<Avatar
-										alt="Sarah Lincoln"
-										src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-									/>
-									<Avatar
-										alt="Miguel Smallz"
-										src="https://images.pexels.com/photos/2830332/pexels-photo-2830332.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-									/>
-									<Avatar
-										alt="Tina Smith"
-										src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-									/>
-								</AvatarGroup>
-								{/* </div> */}
+								{TeamMembers ? (
+									<AvatarGroup max={TeamMembers.length}>
+										{TeamMembers.map((member) => {
+											return (
+												<Avatar
+													alt={member.value}
+													// src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+												/>
+											)
+										})}
+									</AvatarGroup>
+								) : null}
 							</div>
 						</div>
 						<Accordian />
 					</div>
 				</div>
 			</div>
-		);
+		)
 	}
 }
 
 InputSignerData.propTypes = {
 	classes: PropTypes.object.isRequired,
-};
+}
 
-export default withRouter(withStyles(useStyles)(InputSignerData));
+export default withRouter(withStyles(useStyles)(InputSignerData))
