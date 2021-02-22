ef4225291d526cf242d9ec9de4860efb6e658f4e
Miguel Diaz <diaz4674@gmail.com>
2021-02-22 19:47:38 +0000

    added comments

--- poppy-central/src/modules/iterateSigners/index.js
+++ poppy-central/src/modules/iterateSigners/index.js
@@ -1,9 +1,11 @@
 export const iterateSigners = (totalSigners, Signers) => {
     let signers = {}
     for (let i = 1; i < totalSigners + 1; i++) {
+        // check to see if filled in signers match up the total signers stated
         if (Signers[`signer${+i}`] === undefined) {
             return alert("Sorry looks like your still missing some info")
         }
+        // creates signer object key as signer1, signer2, etc...needed to send to backend
         signers[`signer${+i}`] = Signers[`signer${+i}`]
     }
 
